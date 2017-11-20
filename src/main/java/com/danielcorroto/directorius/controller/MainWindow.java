b96f43c935e95509a931d58916b97eacafd0077c
Daniel Corroto <danicq85@gmail.com>
2017-11-20 18:29:55 +0100

    Aplicación básica JavaFX

---
+++ src/main/java/com/danielcorroto/directorius/controller/MainWindow.java
@@ -0,0 +1,32 @@
+package com.danielcorroto.directorius.controller;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Gestión la ventana principal
+ * 
+ * @author Daniel Corroto Quirós
+ *
+ */
+@SuppressWarnings("restriction")
+public class MainWindow extends Application {
+	public MainWindow() {
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		StackPane root = new StackPane();
+		Scene scene = new Scene(root, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
